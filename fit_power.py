06ddcef014b628d6368f9ffb5e10104f4c0ae309
xdze2 <xdze2.me@gmail.com>
2025-09-17 13:36:32 +0200

    Demo merge tree on min height

--- fit_power.py
+++ fit_power.py
@@ -11,27 +11,27 @@ from miniha.rect_stack import compute_horizontal_rectangles
 
 
 
-def generate_random_data():
-    n_points = 200
-    steps = np.random.randn(n_points)
-    Y = np.cumsum(steps)
-    Y = np.array(Y - np.min(Y) + 1)
-    X = np.sort(np.cumsum(1 + 0.5 * np.random.randn(n_points)))
-    X = X - np.min(X)
-    return X, Y
+# def generate_random_data():
+#     n_points = 200
+#     steps = np.random.randn(n_points)
+#     Y = np.cumsum(steps)
+#     Y = np.array(Y - np.min(Y) + 1)
+#     X = np.sort(np.cumsum(1 + 0.5 * np.random.randn(n_points)))
+#     X = X - np.min(X)
+#     return X, Y
 
 input_file = "images/power_2025-09-09.csv"
 print(f"load dataframe from {input_file}")
 df = pd.read_csv(input_file)
 
 
-df = df[:150]
+df = df[:]
 df["date"] = pd.to_datetime(df["date"])
 X_hours = (df["date"] - df["date"][0]).dt.total_seconds() / (60 * 60)
 Y = df["power"]
 
 
-rects = compute_horizontal_rectangles(X_hours, Y)
+rects = compute_horizontal_rectangles(X_hours, Y, base_height=250)
 print(f"First {len(rects)} Rectangles (x_start, x_end, y_start, y_end):")
 for r in rects[:5]:
     print(r)
@@ -39,7 +39,7 @@ for r in rects[:5]:
 print("Plot curve with rectangles...")
 fig, ax = plt.subplots(figsize=(12, 6))
 
-ax.plot(X_hours, Y, marker=".", label="samples", alpha=0.7)
+ax.plot(X_hours, Y, marker="None", label="samples", alpha=0.7)
 # ax.plot(X_half, Y_half, linestyle="None", marker="x", label="middle points", alpha=0.7)
 
 
@@ -54,7 +54,7 @@ for x0, x1, y0, y1, area in rects:
 ax.set_title("Horizontal Rectangle Decomposition")
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
-# ax.set_yscale("log")
+ax.set_yscale("log")
 ax.legend()
 output_file = "fit502.png"
 plt.savefig(output_file)
--- miniha/rect_stack.py
+++ miniha/rect_stack.py
@@ -1,9 +1,36 @@
 import numpy as np
+from typing import List
+from dataclasses import dataclass
+
+
+@dataclass
+class Rect:
+    left_idx: int
+    right_idx: int
+    base_height: float
+    top_height: float
+    parent: "Rect" = None
+
+
+@dataclass
+class RelativeRect:
+    left_idx: int
+    right_idx: int
+    height: float
+    base_height: float = None
+    parent: "Rect" = None
+    removed: bool = False
+
+    def get_base_height(self) -> float:
+        if self.base_height is not None:
+            return self.base_height
+        else:
+            return self.parent.get_base_height() + self.parent.height
 
 
 def horizontal_rectangles_idx(
-    Y, left_idx: int = None, right_idx: int = None, base_height: float = 0
-) -> list:
+    Y, left_idx: int = None, right_idx: int = None, base_height: float = 0, parent_rect: Rect = None
+) -> List[RelativeRect]:
     """
     Recursive horizontal stacking of rectangles to fill curve area.
     Returns a list of rectangles: (x_start, x_end, y_start, y_end)
@@ -19,16 +46,18 @@ def horizontal_rectangles_idx(
     min_height = Y[min_idx]
 
     # Rectangle spans whole interval at the valley height above base
-    rect = (left_idx, right_idx, base_height, min_height)
+    height = min_height - base_height
+    base_height_n = base_height if parent_rect is None else None
+    rect = RelativeRect(left_idx, right_idx, height, base_height_n, parent=parent_rect)
     rects = [rect]
 
     # Recurse left
     if left_idx < min_idx:
-        rects += horizontal_rectangles_idx(Y, left_idx, min_idx - 1, min_height)
+        rects += horizontal_rectangles_idx(Y, left_idx, min_idx - 1, min_height, parent_rect=rect)
 
     # Recurse right
     if min_idx < right_idx:
-        rects += horizontal_rectangles_idx(Y, min_idx + 1, right_idx, min_height)
+        rects += horizontal_rectangles_idx(Y, min_idx + 1, right_idx, min_height, parent_rect=rect)
 
     return rects
 
@@ -42,12 +71,36 @@ def compute_horizontal_rectangles(X: np.array, Y: np.array, base_height: float =
 
     rects_idx = horizontal_rectangles_idx(Y_half, base_height=base_height)
 
+    merge_rects(rects_idx, 6)
+
     rects = list()
-    for left_idx, right_idx, base_height, top_height in rects_idx:
-        x0 = X[left_idx]
-        x1 = X[right_idx + 1]
-        area = (x1 - x0) * (top_height - base_height)
+    for rect in rects_idx:
+        if rect.removed: continue
+        x0 = X[rect.left_idx]
+        x1 = X[rect.right_idx + 1]
+        base_height = rect.get_base_height()
+        area = (x1 -x0)*rect.height
+        top_height = base_height + rect.height
         rects.append((x0, x1, base_height, top_height, area))
 
     rects.sort(key=lambda x: x[-1], reverse=True)
     return rects
+
+
+
+def merge_rects(rect_list: List[RelativeRect], min_height: float):
+
+    # TODO sort before (greedy)
+    for rect in rect_list:
+        print("add", rect.height, min_height)
+        if rect.height < min_height:
+            if rect.parent is None:
+                continue
+            # add height to parent rect
+            parent_width = rect.parent.right_idx - rect.parent.left_idx
+            rect.parent.height += rect.height*(rect.right_idx-rect.left_idx)/parent_width
+            # TODO merge by area
+            rect.removed = True
+            for rectB in rect_list:
+                if rectB.parent == rect:
+                    rectB.parent = rect.parent
